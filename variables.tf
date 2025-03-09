43be31ca3f25e0bf1c1b1f3b09afa15455e6b3a3
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-03-09 22:23:44 +0530

    tf aws vpc

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "project_name" {
     type = string
-  
 }
+
 variable "environment" {
-    type = string  
+    type = string
 }
 
 variable "vpc_cidr" {
@@ -13,7 +13,8 @@ variable "vpc_cidr" {
 variable "enable_dns_hostnames" {
     default = true
 }
-#optional
+
+# Optional
 variable "common_tags" {
     default = {}
 }
@@ -21,50 +22,72 @@ variable "common_tags" {
 variable "vpc_tags" {
     default = {}
 }
+
 variable "igw_tags" {
-  default =  {}
+    default = {}
 }
 
 variable "public_subnet_cidrs" {
-   type = list
-   validation {
+    type = list
+    validation {
         condition = length(var.public_subnet_cidrs) == 2
         error_message = "Please provide 2 valid public subnet CIDR"
-   }
+    }
 }
 
 variable "public_subnet_tags" {
-  default = {}
+    default = {}
 }
 
 variable "private_subnet_cidrs" {
-   type = list
-   validation {
+    type = list
+    validation {
         condition = length(var.private_subnet_cidrs) == 2
         error_message = "Please provide 2 valid private subnet CIDR"
-   }
+    }
 }
 
 variable "private_subnet_tags" {
-  default = {}
+    default = {}
 }
 
 variable "database_subnet_cidrs" {
-   type = list
-   validation {
+    type = list
+    validation {
         condition = length(var.database_subnet_cidrs) == 2
         error_message = "Please provide 2 valid database subnet CIDR"
-   }
+    }
 }
 
 variable "database_subnet_tags" {
-  default = {}
+    default = {}
 }
 
 variable "db_subnet_group_tags" {
-  default = {}
+    default = {}
 }
 
 variable "nat_gateway_tags" {
-  default = {}
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    default = {}
+}
+
+variable "is_peering_required" {
+    type = bool
+    default = false
+}
+
+variable "vpc_peering_tags" {
+    default = {}
 }
